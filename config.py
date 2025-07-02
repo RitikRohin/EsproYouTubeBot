b10a2abfba56a336e697521c96265d6c207b5279
RitikRohin <irthaiwan@gmail.com>
2025-07-03 00:21:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-ADMIN_ID = int(os.environ.get("ADMIN_ID", "123456"))
+ADMIN_ID = int(os.environ.get("ADMIN_ID", "7666870729"))
 
 KEY_FILE = "apikeys.json"
 ALLOWED_FILE = "allowed.json"
