f1fcca008f9ef75bc98647d64ca7f757b148a5cc
RitikRohin <irthaiwan@gmail.com>
2025-07-02 22:17:45 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from pyrogram import Client, filters
+import os
+
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+app = Client(
+    "simple_bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+@app.on_message(filters.command("start"))
+async def start_command(client, message):
+    await message.reply_text(
+        f"Hello {message.from_user.first_name}! 👋\n\n"
+        "I'm alive on Heroku using Pyrogram!"
+    )
+
+app.run()
