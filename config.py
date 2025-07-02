d8427dd69e1eba01e9abf9e8feba4cd41ec15176
RitikRohin <irthaiwan@gmail.com>
2025-07-02 23:45:04 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+ADMIN_ID = int(os.environ.get("ADMIN_ID", "123456"))
+
+KEY_FILE = "apikeys.json"
+ALLOWED_FILE = "allowed.json"
